a15f1a51eec16a42cd107b817c601a2735b8b5cb
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2024-02-15 17:13:21 +0100

    Phone reviews

---
+++ fronted/index.js
@@ -0,0 +1,215 @@
+var phones = [
+    {
+        model: 'Samsung Galaxy Note 20 Ultra',
+        phoneImage: 'samsung.jpg',
+        
+        characteristics: ['Announced: <br/>Status:',
+         '2020, August 05<br/>Available. 2020, August 21',
+        
+        'Operative system:<br/>Chipset:<br/>CPU:<br/>GPU:',
+         'Android 10, One UI 2.5<br/>Exynos 990 (7 nm+)<br/>Octa-core (2x2.73 GHz Mongoose M5 / 2x2.50 GHz Cortex-A76 / 4x2.0 GHz Cortex-A55) <br/>Mali-G77 MP11',
+        
+        'Internal:<br/>RAM:',
+        '128 GB/256 GB/512 GB<br/>8 GB',
+        
+        'Type: Li-Ion 4500 mAh',
+         'Charging: Fast charging 25W',
+        
+        'Main camera:<br/>108 MP, f/1.8, 26mm (wide), 1/1.33 , 0.8µm, PDAF, Laser AF, OIS<br/> 12 MP, f/3.0, 120mm (periscope telephoto), 1.0µm, PDAF, OIS, 5x optical zoom, 50x hybrid zoom<br/> 12 MP, f/2.2, 120˚, 13mm (ultrawide), 1/2.55, 1.4µm"',
+         'front camera:<br/>10 MP, f/2.2, 26mm (wide), 1/3.2, 1.22µm, Dual Pixel PDAF',
+        
+        'Type:<br/>Size:',
+         'Dynamic AMOLED 2X, 120Hz, HDR10+<br/>6.9 inches, 116.7 cm2 (~91.7% screen-to-body ratio)',
+        
+         'Colors:<br/>Models:<br/>price:',
+         'Mystic Bronze, Mystic Black, Mystic White<br/>SM-N985F, SM-N985F/DS<br/>$ 934.99 / £ 1,221.00',],
+        
+        image1: 'samsung.jpg',
+        image2: 'samsung1.jpg',
+        image3: 'samsung2.jpg',
+        
+        name_review: 'Samsung Galaxy Note 20 Ultra review',
+        author: 'Author: Jessica Dolcourt',
+        review: 'If you’re looking for Samsung’s new Galaxy Note 20 Ultra to be a do-everything phone, you won’t be disappointed. Like other Note phones that came before it, the Note 20 Ultra is aimed at a rarified group of Android ’power users’ who want a phone with all the toys and aren’t afraid of a higher price tag. With the Ultra, there’s plenty of both. Samsung saved its best features for the Galaxy Note 20 Ultra, including a more refined design than the Galaxy S20 Ultra -- a phone I don’t recommend. You’ll find an exceptional 6.9-inch screen, sharp 5x optical zoom camera and a swifter stylus for annotating screenshots and taking notes. The Note 20 Ultra also makes small but significant enhancements over the Note 10 Plus, especially in the camera realm. Do these features justify the Note 20 Ultra’s price? It begins at $1,300 (£1,179, AU$1,894) for the 128GB version (you can also buy it in 512GB). The retail price is a steep ask, especially when you combine a climate of deep global recession and mounting unemployment. Add that to the fact that if your active work and social lifestyle has changed at all, like mine has, you won’t truly take advantage of all the features this phone has to offer.  Will the Note 20 Ultra still be the phone you want to use post-pandemic, or will a better upgrade come along by then? ... itd. tekst realno nije bitan',
+        
+        video: 'https://www.youtube.com/embed/R94ntpWVelw'
+            
+    },
+    {
+        model: 'iPhone 12 pro',
+        phoneImage: 'iphone.jpg',
+        
+        characteristics: ['Announced: <br/>Status:',
+         '2020, October 13<br/>Available. 2020, October 23',
+        
+        'Operative system:<br/>Chipset:<br/>CPU:<br/>GPU:',
+         'iOS 14.1, upgradable to iOS 14.2<br/>Apple A14 Bionic (5 nm)<br/>Hexa-core (2x3.1 GHz Firestorm + 4x1.8 GHz Icestorm) <br/>Apple GPU (4-core graphics)',
+        
+        'Internal:<br/>RAM:',
+        '128 GB/256 GB/512 GB<br/>6 GB',
+        
+        'Type: Li-Ion 2815 mAh, non-removable (10.78 Wh)',
+         'Charging: Fast charging 20W, 50% in 30 min',
+        
+        'Main camera:<br/>12 MP, f/1.6, 26mm (wide), 1.4µm, dual pixel PDAF, OIS<br/> 12 MP, f/2.0, 52mm (telephoto), 1/3.4, 1.0µm, PDAF, OIS, 2x optical zoom<br/> 12 MP, f/2.4, 120˚, 13mm (ultrawide), 1/3.6, TOF 3D LiDAR scanner (depth)',
+         'front camera:<br/>12 MP, f/2.2, 23mm (wide), 1/3.6',
+        
+        'Type:<br/>Size:',
+         'Super Retina XDR OLED, HDR10, 800 nits (typ), 1200 nits (peak)<br/>6.1 inches, 90.2 cm2 (~86.0% screen-to-body ratio)',
+        
+         'Colors:<br/>Models:<br/>price:',
+         'Silver, Graphite, Gold, Pacific Blue<br/>A2407, A2341, A2406, A2408<br/>$ 999.00 / € 1,119.00 / £ 999.00',],
+        
+        image1: 'iphone.jpg',
+        image2: 'iphone1.jpg',
+        image3: 'iphone2.jpg',
+        
+        name_review: 'iPhone 12 pro review',
+        author: 'Author: Patrick Holland',
+        review: 'The iPhone 12 Pro Max follows in the footsteps of the 7 Plus and gets camera upgrades that none of the other iPhone 12 models have. At its core the iPhone 12 Pro Max, like all the phones in the iPhone 12 family (the iPhone 12, iPhone 12 Pro and iPhone 12 Mini), has a bunch of things going for it like a new flat-sided design reminiscent of the iPhone 5 and iPad Pro. It has support for 5G, an OLED screen with support for HDR, a ceramic shield covering, an A14 Bionic processor, support for MagSafe wireless charging and it can be submerged to a depth of 6 meters (just under 20 feet) for up to 30 minutes. If you want a deeper assessment of any of these features, take a look at my iPhone 12 and 12 Pro review. ... itd. tekst realno nije bitan',
+        
+        video: 'https://www.youtube.com/embed/eWI_BtcDJu0'
+            
+        },
+    
+    {
+        model: 'One Plus 8 pro',
+        phoneImage: 'oneplus.jpg',
+        
+        characteristics: ['Announced: <br/>Status:',
+         '2020, April 14<br/>Available. Released 2020, April 21',
+        
+        'Operative system:<br/>Chipset:<br/>CPU:<br/>GPU:',
+         'Android 10, upgradable to Android 11, OxygenOS 11<br/>Qualcomm SM8250 Snapdragon 865 (7 nm+)<br/>Octa-core (1x2.84 GHz Kryo 585 / 3x2.42 GHz Kryo 585 / 4x1.8 GHz Kryo 585) <br/>Adreno 650',
+        
+        'Internal:<br/>RAM:',
+        '128 GB/256 GB<br/>8 GB/12GB',
+        
+        'Type: Li-Po 4510 mAh, non-removable',
+         'Charging: Fast charging 30W, 50% in 23 min',
+        
+        'Main camera:<br/>48 MP, f/1.8, 25mm (wide), 1/1.43", 1.12µm, omnidirectional PDAF, Laser AF, OIS<br/> 8 MP, f/2.4, (telephoto), 1/1.0µm, PDAF, OIS, 3x optical zoom<br/> 48 MP, f/2.2, 14mm, 116˚ (ultrawide), 1/2.0", 0.8µm, PDAF<br/>5 MP, f/2.4, (Color filter camera)',
+         'front camera:<br/>16 MP, f/2.5, (wide), 1/3.06", 1.0µm',
+        
+            'Type:<br/>Size:',
+         'Fluid AMOLED, 1B colors, 120Hz, HDR10+<br/>6.78 inches, 111.7 cm2 (~90.8% screen-to-body ratio)',
+        
+         'Colors:<br/>Models:<br/>price:',
+         'Glacial Green, Ultramarine Blue, Onyx Black<br/>IN2023, IN2020, IN2021, IN2025<br/>$ 739.99 / € 720.08 / £ 599.10',],
+    
+        image1: 'oneplus.jpg',
+        image2: 'oneplus1.jpg',
+        image3: 'oneplus2.jpg',
+        
+        name_review: 'OnePlus 8 Pro review: a totally different camera set',
+        author: 'Author: Oliver Cragg',
+            
+        review: 'Technically, the phone’s Snapdragon 865 processor has been surpassed by Qualcomm’s gaming-optimized Snapdragon 865 Plus chipset. Where previously the OnePlus 8 Pro smashed benchmarks including our own Speed Test G, the recent wave of 865 Plus-powered gaming phones like the almighty Asus ROG Phone 3 have gained a small advantage in GPU tests. ... itd. tekst realno nije bitan',
+        
+        video: 'https://www.youtube.com/embed/LTDuGU38-70'
+        
+    }
+];
+function grahFunkcija(i){
+    localStorage.setItem('lokalna', i);
+}
+var koji = localStorage.getItem('lokalna');
+
+function loadPhones(){
+    document.title = phones[koji].model;
+    $('#naziv')[0].innerHTML = phones[koji].model;
+    $('#slika')[0].src=phones[koji].phoneImage;
+    
+    var tabela = $('#tabela')[0];
+    var lista = ['Launch','Platform','Memory','Batery','Camera','Screen','Other'];
+    var j=0;
+    for(i=0;i<lista.length;i++)
+    {
+        var row = tabela.insertRow();
+        
+        var cell = row.insertCell();
+        var cell1 = row.insertCell();
+        cell.innerHTML = phones[koji].characteristics[j];
+        j++;
+        cell1.innerHTML = phones[koji].characteristics[j];
+        j++;
+    }
+    $('#slika1')[0].src=phones[koji].image1;
+    $('#slika2')[0].src=phones[koji].image2;
+    $('#slika3')[0].src=phones[koji].image3;
+    $('#name_review')[0].innerHTML = phones[koji].name_review;
+    $('#author')[0].innerHTML = phones[koji].author;
+    $('#review')[0].innerHTML = phones[koji].review;
+    $('#video')[0].src = phones[koji].video;
+}
+
+function addComment(){
+    var name = $('#InputName').val();
+    var comment = $('#InputComment').val();
+    var o = document.querySelector('input[name="star"]:checked').value;
+    
+    var commenstsection = document.getElementById("commentsection");
+    
+    var newcommentsection = document.createElement("DIV");
+    newcommentsection.setAttribute("class", "comment");
+    
+    var commentTitle = document.createElement("H4");
+    var title = document.createTextNode(name);
+    commentTitle.appendChild(title);
+    
+    var commentContent = document.createElement("P");
+    var content = document.createTextNode(comment);
+    commentContent.appendChild(content);
+    
+    var buttonForReply = document.createElement("button");
+    buttonForReply.nodeType = "button";
+    buttonForReply.setAttribute("class","btn btn-info");
+    buttonForReply.setAttribute("onclick","replyOnComment()");
+    buttonForReply.innerHTML = 'reply';
+    
+    /*var buttonForLike = document.createElement("button1");
+    buttonForReply.nodeType = "button";
+    buttonForReply.setAttribute("class","btn btn-success");
+    buttonForReply.innerHTML = 'LIKE';
+    
+    var buttonForDislike = document.createElement("button2");
+    buttonForReply.nodeType = "button";
+    buttonForReply.setAttribute("class","btn btn-danger");
+    buttonForReply.innerHTML = 'DISLIKE';*/
+   
+    var ocena = document.createElement("H4");
+    ocena.setAttribute("class","ocena");
+    content =  document.createTextNode(o);
+    ocena.appendChild(content);
+    
+    newcommentsection.appendChild(commentTitle);
+    newcommentsection.appendChild(commentContent);
+    newcommentsection.appendChild(ocena);
+    newcommentsection.appendChild(buttonForReply);
+    
+    commenstsection.appendChild(newcommentsection);
+}
+
+function replyOnComment(){
+    var comments = document.querySelectorAll("comment");  
+    var name = $('#InputName').val();
+    var comment = $('#InputComment').val();
+}
+
+function getAverage(){
+    var b=0;
+    var comments = document.getElementById("commentsection");
+    var comment = document.getElementById("comment");
+    var ocene = comments.querySelectorAll(".ocena");
+    for(i=0;i<ocene.length;i++)
+    {
+         b = b + parseFloat(ocene[i].innerHTML);   
+    }
+    document.getElementById("prosecna").innerHTML = b/ocene.length;
+}       
+
+
+
+
+
+
